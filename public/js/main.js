a23f32ad7ef5b473f7b6ef80b3195c41a7c2e1bd
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2021-03-22 00:30:18 +0100

    Background Image Working, Form Add Portal, Data Url conversion

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,35 @@
 const app = new Vue({
     el : "#app",
     data : {
+        displayFormPortal : false,
         displayUI : false,
+        backgroundImgPath : "https://homepages.cae.wisc.edu/~ece533/images/airplane.png",
+        newPortal :{},
     },
     methods : {
+
+        initForm : function (){
+            this.newPortal = {
+                name : "unPortal",
+                color : "loremRouge",
+                imgDataUrl : "",
+            }
+        },
+        async portalImageChanged(event){
+            let blob = await fetch(event.target.files.item(0)).then(r => r.blob());
+            let dataUrl = await new Promise(resolve => {
+                let reader = new FileReader();
+                reader.onload = () => resolve(reader.result);
+                reader.readAsDataURL(blob);
+            });
+            this.newPortal.imgDataUrl = dataUrl
+            console.log(dataUrl);
+        },
+        backgroundImageChanged(event){
+            console.log(event.target.files)
+            this.backgroundImgPath = URL.createObjectURL(event.target.files.item(0));
+        },
+
         ajax: function(url, params = { } ) {
             let s = url+"?";
             for(let key in params) {
